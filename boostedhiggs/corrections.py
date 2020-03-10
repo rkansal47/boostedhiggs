86cb7b8ebce756dfbcbf1ddd52dab8842788e620
Cristina <criss.ms7@gmail.com>
2020-03-10 13:57:02 -0500

    add private dataset that lives in lxplus

--- boostedhiggs/corrections.py
+++ boostedhiggs/corrections.py
@@ -4,7 +4,7 @@ import awkward as ak
 from coffea.util import load
 
 compiled = load(os.path.join(os.path.dirname(__file__), 'data', 'corrections.coffea'))
-
+compiled['2017_pileupweight']._values = np.minimum(5, compiled['2017_pileupweight']._values)
 
 def _msoftdrop_weight(pt, eta):
     gpar = np.array([1.00626, -1.06161, 0.0799900, 1.20454])
@@ -17,37 +17,22 @@ def _msoftdrop_weight(pt, eta):
     weight = np.where(np.abs(eta) < 1.3, cenweight, forweight)
     return genw*weight
 
-
 def corrected_msoftdrop(fatjets):
     sf_flat = _msoftdrop_weight(fatjets.pt.flatten(), fatjets.eta.flatten())
     sf_flat = np.maximum(1e-5, sf_flat)
-    try:
-        # old pancakes
-        dazsle_msd = fatjets.msoftdrop_raw
-    except AttributeError:
-        dazsle_msd = (fatjets.subjets * (1 - fatjets.subjets.rawFactor)).sum().mass
+    dazsle_msd = (fatjets.subjets * (1 - fatjets.subjets.rawFactor)).sum().mass
     return dazsle_msd * ak.JaggedArray.fromoffsets(fatjets.array.offsets, sf_flat)
-
 
 def n2ddt_shift(fatjets, year='2017'):
     return compiled[f'{year}_n2ddt_rho_pt'](fatjets.rho, fatjets.pt)
 
-
-def add_pileup_weight(weights, nPU, year='2017', dataset=None):
-    if year == '2017' and dataset in compiled['2017_pileupweight_dataset']:
-        weights.add(
-            'pileup_weight',
-            compiled['2017_pileupweight_dataset'][dataset](nPU),
-            compiled['2017_pileupweight_dataset_puUp'][dataset](nPU),
-            compiled['2017_pileupweight_dataset_puDown'][dataset](nPU),
-        )
-    else:
-        weights.add(
-            'pileup_weight',
-            compiled[f'{year}_pileupweight'](nPU),
-            compiled[f'{year}_pileupweight_puUp'](nPU),
-            compiled[f'{year}_pileupweight_puDown'](nPU),
-        )
+def add_pileup_weight(weights, nPU, year):
+    weights.add(
+        'pileup_weight',
+        compiled[f'{year}_pileupweight'](nPU),
+        compiled[f'{year}_pileupweight_puUp'](nPU),
+        compiled[f'{year}_pileupweight_puDown'](nPU),
+    )
 
 
 def add_VJets_NLOkFactor(weights, genBosonPt, year, dataset):
